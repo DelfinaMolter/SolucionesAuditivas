0a7895d008d074d3395e017ef6be9447a6086f1e
DelfinaMolter <delfi.molter@gmail.com>
2023-06-22 00:24:24 -0300

    footer ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,21 @@
-
+import "./Footer.css";
 import imgLogo from './../assets/img/LogoDefault.png';
 
 function Footer(){
     return(
-        <footer>
-            <img src={imgLogo} alt="Logo" />
-            <p>Distribución y exportación de dispositivos auditivos e implantables.</p>
-            <div>
-                <a href="#i">Contacto</a>
-                <p>Building 9090 Unit 6, Panamá</p>
-                <p>soluciones.auditivas@solucionesauditivas.com.pa</p>
+        <footer >
+            <div className='container'>
+                <div className='d-flex justify-content-between align-items-start align-items-md-center gap-5'>
+                    <img src={imgLogo} alt="Logo" className='d-none d-md-block my-4'/>
+                    <p className='d-none d-lg-block slogan'>Distribución y exportación de <span> dispositivos auditivos e implantables</span>.</p>
+                    <div className='contact'>
+                        <h5>
+                            <a href="#i" >Contacto</a>
+                        </h5>
+                        <p  className='my-2'>Building 9090 Unit 6, Panamá</p>
+                        <p  className='my-2'>soluciones.auditivas@solucionesauditivas.com.pa</p>
+                    </div>
+                </div>
             </div>
         </footer>
     )
--- src/components/NavbarHeader.jsx
+++ src/components/NavbarHeader.jsx
@@ -2,7 +2,6 @@ import { Button } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Offcanvas from 'react-bootstrap/Offcanvas';
 import imgLogo from './../assets/img/logoDefaultHeader.png';
 import imgFlagPanama from './../assets/img/panama-flag.png';
 import imgFlagUSA from './../assets/img/usa-flag.png';
@@ -21,8 +20,8 @@ function NavbarHeader(){
           <Container fluid>
             <Navbar.Brand href="#"><img src={imgLogo} alt="Logo" /></Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end align-items-center flex-grow-1">
-              <Nav className="mr-auto align-items-center">
+            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end align-items-center  flex-grow-1">
+              <Nav className="mr-auto align-items-start align-items-md-center">
                 <Nav.Link href="#features">Nosotros</Nav.Link>
                 <Nav.Link href="#pricing">Productos</Nav.Link>
                 <Button>Contacto</Button>
@@ -32,57 +31,9 @@ function NavbarHeader(){
                 <img src={imgFlagUSA} alt="USA Flag" className={activeFlag === 'USA' && 'active'} onClick={()=>{handleFlag('USA')}} />
               </Nav>
             </Navbar.Collapse>
-            {/* <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-md`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-              placement="top"
-            >
-              <Offcanvas.Header closeButton />
-              <Offcanvas.Body>
-                <Nav className="justify-content-end align-items-center flex-grow-1">
-                  <Nav.Link href="#action1">Nosotros</Nav.Link>
-                  <Nav.Link href="#action2">Productos</Nav.Link>
-                  <Button>Contacto</Button>
-                  <div className="flag">
-                    <img src={imgFlagPanama} alt="Panama Flag" className={activeFlag === 'PAN' && 'active'} onClick={()=>{handleFlag('PAN')}} />
-                    <img src={imgFlagUSA} alt="USA Flag" className={activeFlag === 'USA' && 'active'} onClick={()=>{handleFlag('USA')}} />
-                  </div>
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas> */}
+            
           </Container>
         </Navbar>
-
-      {/* <a href="#7"><img src={imgLogo} alt="Logo" /></a> */}
-
-      {/* <div className='d-none d-md-block'>
-          <a href="#action1">Nosotros</a>
-          <a href="#action2">Productos</a>
-          <Button>Contacto</Button>
-        <div className="flag">
-          <img src={imgFlagPanama} alt="Panama Flag" className={activeFlag === 'PAN' && 'active'} onClick={()=>{handleFlag('PAN')}} />
-          <img src={imgFlagUSA} alt="USA Flag" className={activeFlag === 'USA' && 'active'} onClick={()=>{handleFlag('USA')}} />
-        </div>
-      </div> */}
-
-      {/* <div className="d-md-none navbar-top">
-        <div className="toggle-menu">
-          <input type="checkbox" id="burger-check" />
-          <div className="burger-layer"></div>
-          <div className="burger-layer"></div>
-          <div className="burger-layer"></div>
-          <div className="menu-top">
-              <a href="#action1" className="menu-item">Nosotros</a>
-              <a href="#action2" className="menu-item">Productos</a>
-              <Button className="menu-item">Contacto</Button>
-            <div className="flag">
-              <img src={imgFlagPanama} alt="Panama Flag" className={`menu-item ${activeFlag === 'PAN' && 'active'}`} onClick={()=>{handleFlag('PAN')}} />
-              <img src={imgFlagUSA} alt="USA Flag" className={`menu-item ${activeFlag === 'USA' && 'active'}`} onClick={()=>{handleFlag('USA')}} />
-            </div>
-          </div>
-        </div>
-
-      </div> */}
       </div>
     )
 }
